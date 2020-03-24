0629091244ad1326198780bc98d3bbf4ad736979
Michael Hatmaker <michaeldhatmaker@gmail.com>
2020-03-24 15:50:43 -0500

    some IronPython and Visual Studio Extension code

---
+++ VSExtensionDemo/VSExtensionDemo/CommandLineWrap.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Globalization;
+using System.Threading;
+using System.Threading.Tasks;
+using EnvDTE;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Task = System.Threading.Tasks.Task;
+
+
+//  https://www.fmtconsultants.com/creating-a-visual-studio-extension-part-2-of-2/
+
+
+namespace VSExtensionDemo
+{
+    /// <summary>
+    /// Command handler
+    /// </summary>
+    internal sealed class CommandLineWrap
+    {
+        /// <summary>
+        /// Command ID.
+        /// </summary>
+        public const int CommandId = 0x0100;
+
+        /// <summary>
+        /// Command menu group (command set GUID).
+        /// </summary>
+        public static readonly Guid CommandSet = new Guid("fc1b7d77-444c-4bae-9f79-0a4dd8e4fcac");
+
+        /// <summary>
+        /// VS Package that provides this command, not null.
+        /// </summary>
+        private readonly AsyncPackage package;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CommandLineWrap"/> class.
+        /// Adds our command handlers for menu (commands must exist in the command table file)
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        /// <param name="commandService">Command service to add command to, not null.</param>
+        private CommandLineWrap(AsyncPackage package, OleMenuCommandService commandService)
+        {
+            this.package = package ?? throw new ArgumentNullException(nameof(package));
+            commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+
+            var menuCommandID = new CommandID(CommandSet, CommandId);
+            var menuItem = new MenuCommand(this.Execute, menuCommandID);
+            commandService.AddCommand(menuItem);
+        }
+
+        /// <summary>
+        /// Gets the instance of the command.
+        /// </summary>
+        public static CommandLineWrap Instance
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the service provider from the owner package.
+        /// </summary>
+        private Microsoft.VisualStudio.Shell.IAsyncServiceProvider ServiceProvider
+        {
+            get
+            {
+                return this.package;
+            }
+        }
+
+        /// <summary>
+        /// Initializes the singleton instance of the command.
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        public static async Task InitializeAsync(AsyncPackage package)
+        {
+            // Switch to the main thread - the call to AddCommand in CommandLineWrap's constructor requires
+            // the UI thread.
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
+
+            OleMenuCommandService commandService = await package.GetServiceAsync(typeof(IMenuCommandService)) as OleMenuCommandService;
+            Instance = new CommandLineWrap(package, commandService);
+        }
+
+        /// <summary>
+        /// This function is the callback used to execute the command when the menu item is clicked.
+        /// See the constructor to see how the menu item is associated with this function using
+        /// OleMenuCommandService service and MenuCommand class.
+        /// </summary>
+        /// <param name="sender">Event sender.</param>
+        /// <param name="e">Event args.</param>
+        private void Execute(object sender, EventArgs e)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            string message = string.Format(CultureInfo.CurrentCulture, "Inside {0}.MenuItemCallback()", this.GetType().FullName);
+            string title = "CommandLineWrap";
+
+            //var mcs = this.ServiceProvider.GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
+            //OleMenuCommandService commandService = await package.GetServiceAsync(typeof(IMenuCommandService)) as OleMenuCommandService;
+            var dte = this.ServiceProvider.GetService(typeof(EnvDTE._DTE)) as EnvDTE._DTE;
+            var activeDocument = dte.ActiveDocument;
+            if (activeDocument == null) return;
+
+            var textDocument = activeDocument.Object() as TextDocument;
+            if (textDocument == null) return;
+            var text = textDocument.CreateEditPoint(textDocument.StartPoint).GetText(textDocument.EndPoint);
+
+
+            // Show a message box to prove we were here
+            VsShellUtilities.ShowMessageBox(
+                this.package,
+                //message,
+                text,
+                title,
+                OLEMSGICON.OLEMSGICON_INFO,
+                OLEMSGBUTTON.OLEMSGBUTTON_OK,
+                OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+        }
+    }
+}
---
+++ master/IronPythonTutorials-master/README.md
@@ -0,0 +1,34 @@
+# NeedfulSoftware - IronPython Tutorials
+
+This repository contains the examples for our [IronPython tutorials](http://www.needfulsoftware.com/IronPython).
+
+## List of examples
+
+### C# integration
+- [PythonScriptExecution1](https://github.com/NeedfulSoftware/IronPythonTutorials/tree/master/CSharpIntegration/PythonScriptExecution1): Example showing how to execute a script hardcoded as a string.
+- [PythonScriptExecution2](https://github.com/NeedfulSoftware/IronPythonTutorials/tree/master/CSharpIntegration/PythonScriptExecution2): Example showing how to execute a script from a file.
+- [PythonScriptExecution3](https://github.com/NeedfulSoftware/IronPythonTutorials/tree/master/CSharpIntegration/PythonScriptExecution3): Example showing how to execute scripts that import modules.
+- [PythonScriptExecution4](https://github.com/NeedfulSoftware/IronPythonTutorials/tree/master/CSharpIntegration/PythonScriptExecution4): Example showing how to use the standard library.
+
+
+## License
+
+Copyright (c) 2015-2017 Xavier Leclercq
+
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+IN THE SOFTWARE.
